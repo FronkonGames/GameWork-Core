4462d0ef5a1646ec6cbdfe6bce319ceb07a31b27
Martin Bustos <fronkongames@gmail.com>
2022-05-09 02:23:23 +0200

    Modules logic.

--- Runtime/Game.cs
+++ Runtime/Game.cs
@@ -14,6 +14,8 @@
 // COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
 // OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+using System;
+using UnityEngine;
 using FronkonGames.GameWork.Foundation;
 
 namespace FronkonGames.GameWork.Core
@@ -21,13 +23,177 @@ namespace FronkonGames.GameWork.Core
   /// <summary>
   /// 
   /// </summary>
-  public abstract class Game : MonoBehaviourSingleton<BaseMonoBehaviour>
+  public abstract class Game : MonoBehaviourSingleton<BaseMonoBehaviour>,
+                               IInitializable,
+                               IUpdatable,
+                               IDestructible
   {
+    /// <summary>
+    /// Initialized?
+    /// </summary>
+    /// <value>True/false.</value>
+    public bool Initialized { get; set; }
+
+    /// <summary>
+    /// Should be updated?
+    /// </summary>
+    /// <value>True/false.</value>
+    public bool ShouldUpdate { get; set; } = true;
+
+    /// <summary>
+    /// Will it be destroyed?
+    /// </summary>
+    /// <value>True si va a destruirse.</value>
+    public bool WillDestroy { get; set; }
+
+    private readonly FastList<IInitializable> initializables = new FastList<IInitializable>();
+    private readonly FastList<IActivable> activables = new FastList<IActivable>();
+    private readonly FastList<IUpdatable> updatables = new FastList<IUpdatable>();
+    private readonly FastList<IGUI> GUIables = new FastList<IGUI>();
+    private readonly FastList<IRenderObject> renderableObjects = new FastList<IRenderObject>();
+    private readonly FastList<IDestructible> destructibles = new FastList<IDestructible>();
+#if UNITY_ANDROID || UNITY_IOS
+    private readonly FastList<ILowMemory> lowMemories = new FastList<ILowMemory>();
+#endif
+    private readonly FastList<IModule> allModules = new FastList<IModule>();
+
+    /// <summary>
+    /// On initialize.
+    /// </summary>
+    public virtual void OnInitialize() { }
+
+    /// <summary>
+    /// At the end of initialization.
+    /// Called in the first Update frame.
+    /// </summary>
+    public virtual void OnInitialized() { }
+
+    /// <summary>
+    /// On deinitialize.
+    /// </summary>
+    public virtual void OnDeinitialize() { }
+
+    /// <summary>
+    /// Update event.
+    /// </summary>
+    public virtual void OnUpdate() { }
+
+    /// <summary>
+    /// FixedUpdate event.
+    /// </summary>
+    public virtual void OnFixedUpdate() { }
+
+    /// <summary>
+    /// LateUpdate event.
+    /// </summary>
+    public virtual void OnLateUpdate() { }
+
+    /// <summary>
+    /// On destroy.
+    /// </summary>
+    public virtual void OnWillDestroy() { }
+
+    /// <summary>
+    /// Returns a module.
+    /// </summary>
+    /// <typeparam name="T">Type of module.</typeparam>
+    /// <returns>Module o null</returns>
+    public T GetModule<T>() where T : IModule, new()
+    {
+      Type typeOf = typeof(T);
+
+      for (int i = 0; i < allModules.Count; ++i)
+      {
+        if (allModules[i].GetType() == typeOf)
+          return (T)allModules[i];
+      }
+
+      Log.Exception($"Unable to get '{typeOf}', it was not registerd");
+
+      return default(T);
+    }
+
+    /// <summary>
+    /// Returns a module.
+    /// </summary>
+    /// <typeparam name="T">Type of module.</typeparam>
+    /// <returns>Module o null</returns>
+    public IModule GetModule(Type typeOf)
+    {
+      for (int i = 0; i < allModules.Count; ++i)
+      {
+        if (allModules[i].GetType() == typeOf)
+          return allModules[i];
+      }
+
+      Log.Exception($"Unable to get '{typeOf}', it was not registerd");
+
+      return null;
+    }
+
+    /// <summary>
+    /// The module is registered?
+    /// </summary>
+    /// <typeparam name="T">Type of module.</typeparam>
+    /// <returns>True if you are registered.</returns>
+    public bool HasModule<T>() where T : IModule, new()
+    {
+      Type typeOfS = typeof(T);
+      for (int i = 0; i < allModules.Count; ++i)
+      {
+        if (this.allModules[i].GetType() == typeOfS)
+          return true;
+      }
+
+      return false;
+    }
+
+    private void RegisterModule(IModule module, Type type)
+    {
+      if (typeof(IInitializable).IsAssignableFrom(type) == true)
+        initializables.Add(module as IInitializable);
+
+      if (typeof(IActivable).IsAssignableFrom(type) == true)
+        activables.Add(module as IActivable);
+
+      if (typeof(IUpdatable).IsAssignableFrom(type) == true)
+        updatables.Add(module as IUpdatable);
+
+      if (typeof(IGUI).IsAssignableFrom(type) == true)
+        GUIables.Add(module as IGUI);
+
+      if (typeof(IRenderObject).IsAssignableFrom(type) == true)
+        renderableObjects.Add(module as IRenderObject);
+
+      if (typeof(IDestructible).IsAssignableFrom(type) == true)
+        destructibles.Add(module as IDestructible);
+
+#if UNITY_ANDROID || UNITY_IOS
+      if (typeof(ILowMemory).IsAssignableFrom(type) == true)
+        lowMemories.Add(service as ILowMemory);
+#endif
+
+      allModules.Add(module);
+
+      Log.Info($"'{type.ToString()}' registred");
+    }
+
     /// <summary>
     /// During the awake, this system will start the initialization.
     /// </summary>
     private void Awake()
     {
+      DontDestroyOnLoad(this.gameObject);
+
+      Application.wantsToQuit += OnWantsToQuit;
+#if UNITY_ANDROID || UNITY_IOS
+      Application.lowMemory += OnLowMemory;
+#endif
+
+      this.OnInitialize();
+
+      for (int i = 0; i < initializables.Count; ++i)
+        initializables[i].OnInitialize();
     }
 
     /// <summary>
@@ -35,6 +201,20 @@ namespace FronkonGames.GameWork.Core
     /// </summary>
     private void OnDisable()
     {
+      for (int i = 0; i < initializables.Count; ++i)
+      {
+        if (initializables[i].Initialized == true)
+        {
+          initializables[i].OnDeinitialize();
+          initializables[i].Initialized = false;
+        }
+      }
+
+      if (this.Initialized == true)
+      {
+        this.OnDeinitialize();
+        this.Initialized = false;
+      }
     }
 
     /// <summary>
@@ -42,6 +222,28 @@ namespace FronkonGames.GameWork.Core
     /// </summary>
     private void Update()
     {
+      if (Initialized == false)
+      {
+        this.Initialized = true;
+        this.OnInitialized();
+
+        for (int i = 0; i < initializables.Count; ++i)
+        {
+          initializables[i].Initialized = true;
+          initializables[i].OnInitialized();
+        }
+      }
+
+      if (ShouldUpdate == true && this.WillDestroy == false)
+      {
+        this.OnUpdate();
+
+        for (int i = 0; i < updatables.Count; ++i)
+        {
+          if (updatables[i].ShouldUpdate == true)
+            updatables[i].OnUpdate();
+        }
+      }
     }
 
     /// <summary>
@@ -49,6 +251,16 @@ namespace FronkonGames.GameWork.Core
     /// </summary>
     private void FixedUpdate()
     {
+      if (ShouldUpdate == true && this.WillDestroy == false)
+      {
+        this.OnFixedUpdate();
+
+        for (int i = 0; i < updatables.Count; ++i)
+        {
+          if (updatables[i].ShouldUpdate == true)
+            updatables[i].OnFixedUpdate();
+        }
+      }
     }
 
     /// <summary>
@@ -57,6 +269,16 @@ namespace FronkonGames.GameWork.Core
     /// </summary>
     private void LateUpdate()
     {
+      if (ShouldUpdate == true && this.WillDestroy == false)
+      {
+        this.OnLateUpdate();
+
+        for (int i = 0; i < updatables.Count; ++i)
+        {
+          if (updatables[i].ShouldUpdate == true)
+            updatables[i].OnLateUpdate();
+        }
+      }
     }
 
     /// <summary>
@@ -64,6 +286,8 @@ namespace FronkonGames.GameWork.Core
     /// </summary>
     private void OnRenderObject()
     {
+      for (int i = 0; i < renderableObjects.Count; ++i)
+        renderableObjects[i].OnRenderObject();
     }
 
     /// <summary>
@@ -71,6 +295,32 @@ namespace FronkonGames.GameWork.Core
     /// </summary>
     private void OnDestroy()
     {
+      this.WillDestroy = true;
+
+      for (int i = 0; i < destructibles.Count; ++i)
+      {
+        destructibles[i].WillDestroy = true;
+        destructibles[i].OnWillDestroy();
+      }
+
+      this.OnWillDestroy();
+
+      initializables.Clear();
+      activables.Clear();
+      updatables.Clear();
+      GUIables.Clear();
+      renderableObjects.Clear();
+      destructibles.Clear();
+#if UNITY_ANDROID || UNITY_IOS
+      lowMemories.Clear();
+#endif
+
+      allModules.Clear();
+
+#if UNITY_ANDROID || UNITY_IOS
+      Application.lowMemory -= OnLowMemory;
+#endif
+      Application.wantsToQuit -= OnWantsToQuit;
     }
 
 #if UNITY_EDITOR
@@ -79,6 +329,8 @@ namespace FronkonGames.GameWork.Core
     /// </summary>
     private void OnDrawGizmos()
     {
+      for (int i = 0; i < GUIables.Count; ++i)
+        GUIables[i].OnDrawGizmos();
     }
 #endif
 
@@ -87,6 +339,8 @@ namespace FronkonGames.GameWork.Core
     /// </summary>
     private void OnGUI()
     {
+      for (int i = 0; i < GUIables.Count; ++i)
+        GUIables[i].OnGUI();
     }
 
     /// <summary>
@@ -95,6 +349,11 @@ namespace FronkonGames.GameWork.Core
     protected override void OnApplicationQuit()
     {
       base.OnApplicationQuit();
+
+      this.WillDestroy = true;
+
+      for (int i = 0; i < destructibles.Count; ++i)
+        destructibles[i].WillDestroy = true;
     }
 
 #if UNITY_ANDROID || UNITY_IOS
@@ -103,14 +362,32 @@ namespace FronkonGames.GameWork.Core
     /// </summary>
     private void OnLowMemory()
     {
+      for (int i = 0; i < lowMemories.Count; ++i)
+        lowMemories[i].OnLowMemory();
     }
 #endif
 
-    /// <summary>
-    /// 
-    /// <summary>
     private bool OnWantsToQuit()
     {
+      // @HACK: At the end of the execution, Unity calls OnDisable when there are already non valid objects.
+      // Calling OnDeinitialize here ensures that objects are still valid and can be used.
+      for (int i = 0; i < initializables.Count; ++i)
+      {
+        if (initializables[i].Initialized == true)
+        {
+          initializables[i].OnDeinitialize();
+          initializables[i].Initialized = false;
+        }
+      }
+
+      if (this.Initialized == true)
+      {
+        this.OnDeinitialize();
+        this.Initialized = false;
+      }
+
+      this.WillDestroy = true;
+
       return true;
     }
   }
